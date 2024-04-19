f8c06e1dcdd645f9ad5abb50ec5c1b3c5e7a28b1
Robert Howard <81573756+Robert-Howard1031@users.noreply.github.com>
2024-04-18 20:36:22 -0500

    Delete view directory

--- view/categories.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require("header.php");
-    require("category_list.php");
-    require("add_category_form.php");
-    require("footer.php");
--- view/items.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require("header.php");
-    require("item_list.php");
-    require("add_item_form.php");
-    require("footer.php");
